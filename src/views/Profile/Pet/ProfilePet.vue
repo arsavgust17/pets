ca875cba4ac06ddfc1f345c7729e9d889bf9ff30
Senior-Pomidor <bobylev.vladi@gmail.com>
2019-10-31 19:28:01 +0300

    Add pictures

--- src/views/Profile/Pet/ProfilePet.vue
+++ src/views/Profile/Pet/ProfilePet.vue
@@ -1,6 +1,60 @@
 <template>
     <div>
-        ProfilePet {{id}}
+
+        <div class="link-btn-wrapper--top">
+            <a href="#" class="link-btn">Отмена</a>
+            <a href="#" class="link-btn">Готово</a>
+        </div>
+
+        <el-card class="box-card pet-profile-header">
+            <div class="pet-photo-wrapper">
+                <img class="pet-photo" src="../../../assets/profile-pet-photo.png" alt="User" />
+            </div>
+            <div class="pet-profile-header__user">
+                <div class="td--right">
+                    <h1 class="pet-profile-header__user-name">Хатико</h1>
+                </div>
+                <div class="td--right">Собака</div>
+            </div>
+        </el-card>
+
+        <el-card class="box-card pet-profile-data">
+            <h3 class="box-card__title">Данные питомца</h3>
+            <div class="row">
+                <div class="td--left">Порода</div>
+                <div class="td--right">Бультерьер</div>
+            </div>
+            <div class="row">
+                <div class="td--left">Возраст</div>
+                <div class="td--right">5 лет</div>
+            </div>
+            <div class="row">
+                <div class="td--left">Пол</div>
+                <div class="td--right">Мужской</div>
+            </div>
+            <div class="row">
+                <div class="td--left">Время выгула</div>
+                <div class="td--right">30 мин</div>
+            </div>
+            <div class="row">
+                <div class="td--left">Характер</div>
+                <div class="td--right">Игривый</div>
+            </div>
+            <label for="comment-field" class="comment-field-header">
+                Дополнительные комментарии
+            </label>
+            <textarea id="comment-field" name="comment-field" class="comment-field">Очень подвижный, дружелюбный, &#173; не может жить без активных игр.
+            </textarea>
+        </el-card>
+        <Button class="add-walk-btn">Добавить прогулку</Button>
+        <el-card class="box-card story">
+            <h3 class="box-card__title">История прогулок</h3>
+            <p class="box-card__p">Данные за последний месяц</p>
+            <div class="arrow"></div>
+        </el-card>
+        <div class="link-btn-wrapper--bottom">
+            <a href="#" class="link-btn">Удалить профиль питомца</a>
+        </div>
     </div>
 </template>
 
@@ -16,5 +70,157 @@
 </script>
 
 <style scoped>
+
+    .link-btn-wrapper--top {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    >>>.el-card__body {
+        padding: 19px 0 19px 21px;
+    }
+
+    .pet-profile-header {
+        margin-top: 27px;
+    }
+
+    >>>.pet-profile-header > .el-card__body {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .pet-photo-wrapper {
+        display: inline-flex;
+        position: relative;
+        width: 72px;
+        height: 72px;
+        border: 3px solid #fff;
+        border-radius: 50%;
+        box-shadow: 0 0 5px rgba(22, 36, 83, 0.3);
+        margin: auto 0;
+    }
+
+    .pet-photo {
+        width: 100%;
+        border-radius: 50%;
+    }
+
+    .pet-photo-wrapper::after {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        content: "";
+        display: block;
+        width: 22px;
+        height: 22px;
+        border-radius: 50%;
+        background: #fff url('../../../assets/plus.png') center no-repeat;
+        box-shadow: 0px 10px 34px rgba(22, 36, 83, 0.2);
+    }
+
+    .pet-profile-header__user {
+        width: 210px;
+    }
+
+    .pet-profile-header__user-name {
+        margin: 0;
+    }
+
+    .pet-profile-data {
+        margin-top: 10px;
+    }
+
+    .box-card__title {
+        margin: 0;
+        text-align: left;
+    }
+
+    .row {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 6px;
+    }
+
+    .td--right {
+        width: 210px;
+        height: 44px;
+        text-align: left;
+        line-height: 44px;
+        border-bottom: 1px solid #DFE1E5;
+    }
+
+    .td--left {
+        text-align: left;
+        line-height: 44px;
+        font-size: 14px;
+    }
+
+    .comment-field-header {
+        display: flex;
+        margin-top: 17px;
+        text-align: left;
+        font-size: 14px;
+    }
+
+    .comment-field {
+        width: 100%;
+        resize: none;
+        border: none;
+        border-bottom: 1px solid #DFE1E5;
+        padding: 8px 30px 10px 1px;
+        font-size: 16px;
+        color: #989DAC;
+        opacity: 0.8;
+        outline: 0;
+    }
+
+    .add-walk-btn {
+        width: 100%;
+        height: 61px;
+        margin-top: 25px;
+        font-size: 18px;
+        color: #fff;
+        border: none;
+        outline: none;
+        border-radius: 10px;
+        background-color: #6074E8;
+    }
+
+    .box-card__p {
+        margin: 0;
+        margin-top: 5px;
+        font-size: 14px;
+        line-height: 20px;
+        text-align: left;
+    }
+
+    .story {
+        margin-top: 18px;
+    }
+
+    >>>.story > .el-card__body {
+        position: relative;
+    }
+
+    .arrow {
+        position: absolute;
+        right: 15px;
+        top: 50%;
+        margin-top: -5px;
+        width: 10px;
+        height: 10px;
+        border-right: 1.5px solid #989DAC;
+        border-top: 1.5px solid #989DAC;
+        transform: rotate(45deg);
+    }
+
+    .link-btn {
+        text-decoration: none;
+        color: #6074E8;
+    }
+
+    .link-btn-wrapper--bottom {
+        margin-top: 30px;
+    }
 
 </style>
--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -1,8 +1,60 @@
 <template>
     <div class="profile">
-        <el-card class="box-card">
-            <div v-for="o in 4" :key="o" class="text item">
-                {{'List item ' + o }}
+        <el-card class="box-card pet-profile-header">
+            <div class="pet-photo-wrapper">
+                <img src="../../assets/user-icon.png" alt="User" class="pet-photo"/>
+            </div>
+            <div class="pet-profile-header__user">
+                <div class="profile-name">
+                    <h1 class="pet-profile-header__user-name">{{profile.name}}</h1>
+                </div>
+                <div class="profile-phone">{{profile.phone}}</div>
+                <div class="profile-address">{{profile.address}}</div>
+                <div class="link-btn-wrapper">
+                    <a href="#" @click.prevent="editProfile()" class="link-btn">Редактировать профиль</a>
+                </div>
+            
+            </div>
+        </el-card>
+
+        <el-card v-for="pet in pets" class="box-card">
+            <div class="pet-logo-wrapper">
+                <img class="pet-logo" src="../../assets/Pet-logo.png" alt="Pet-logo">
+            </div>
+            <div>
+                <h3 class="pet-name box-card__title">{{pet.name}}</h3>
+                <div class="pet-type">
+                    <span class="poroda">{{pet.breed.name}}</span>, 
+                    <span class="type">{{pet.type.name}}</span>
+                </div>
+            </div>
+        </el-card>
+
+        <div class="add-pet">
+            <el-button type="success" style="background: #6074E8;" icon="el-icon-plus" circle></el-button>
+            <div class="add-pet-tex">
+                <div class="text">
+                    Добавить питомца
+                </div>
+                <div class="gray-text">
+                    для создания прогулок
+                </div>
+            </div>
+        </div>
+        <el-card class="box-card pet-profile-header">
+            <div class="block">
+                <h3>Рейтинг исполнителя</h3>
+                <div class="profile-rating">
+                    <el-rate
+                            v-model="value"
+                            disabled
+                            show-score
+                            text-color="#ff9900"
+                            score-template=""
+                            allow-half>
+                    </el-rate>
+                    <div class="rating-value">{{Number.parseFloat(value)}}{{Number.parseFloat(value) % 1 ? "" : ".0"}} <div class="gray-text">12 оценок</div></div>
+                </div>
             </div>
         </el-card>
     </div>
@@ -11,28 +63,241 @@
 
 <script>
     import axios from 'axios';
+
     export default {
         name: "Profile",
         data() {
             return {
-                url: 'https://rest.hackmoscow.rainweb.ru/auth/login',
+                value: 5.0,
+                colors: ['#99A9BF', '#F7BA2A', '#FF9900'], // same as { 2: '#99A9BF', 4: { value: '#F7BA2A', excluded: true }, 5: '#FF9900' }
+                url: 'https://rest.hackmoscow.rainweb.ru',
                 login: 'max@hm.ru',
                 password: 'maxx',
-            };
+                "id": "",
+                "email": "",
+                "status": "",
+                "balance": "",
+                "profile": {
+                    "name": "",
+                    "avatar": "",
+                    "nickname": "",
+                    "email": "",
+                    "site": "",
+                    "phone": "",
+                    "address": "",
+                    "rating_customer": "",
+                    "rating_executor": "",
+                    "show_walk": "",
+                    "show_push": "",
+                    "dob": "",
+                    "sex": "",
+                    "experience_communication": ""
+                },
+                pets: [],
+            }
+        },
+        methods: {
+            editProfile() {
+                this.$router.push({name: 'profileEdit'});
+            },
         },
         created() {
             axios({
                 method: 'POST',
-                url: this.url,
+                url: this.url + '/auth/login',
                 data: {
                     login: this.login,
                     password: this.password,
                 },
+            }).then(({ data }) => {
+                window.localStorage.setItem('userToken', data.data.token);
+                window.console.log(window.localStorage.getItem('userToken'));
+                axios({
+                    method: 'POST',
+                    url: this.url + '/user/get/self',
+                    data: {
+                        token: window.localStorage.getItem('userToken'),
+                    }
+                })
+                    .then(({ data }) => {
+                    const user = data.data;
+                    this.id = user.id;
+                    this.email = user.email;
+                    this.status = user.status;
+                    this.balance = user.balance;
+                    this.profile = user.profile;
+                    this.profile.name = user.profile.name;
+                    this.profile.avatar = user.profile.avatar;
+                    this.profile.nickname = user.profile.nickname;
+                    this.profile.email = user.profile.email;
+                    this.profile.site = user.profile.site;
+                    this.profile.phone = user.profile.phone;
+                    this.profile.address = user.profile.address;
+                    this.profile.rating_customer = user.rating_customer;
+                    this.profile.rating_executor = user.rating_executor;
+                    this.profile.show_walk = user.show_walk;
+                    this.profile.show_push = user.show_push;
+                    this.profile.dob = user.dob;
+                    this.profile.sex = user.sex;
+                    this.profile.experience_communication = user.experience_communication;
+                });
+                axios({
+                    method: 'POST',
+                    url: this.url + '/pet/all',
+                    data: {
+                        login: this.login,
+                        password: this.password,
+                    },
+                }).then(({ data }) => {
+                    this.pets = data.data;
+                });
             })
         }
     };
 </script>
 
 <style scoped>
+    a {
+        text-decoration: none;
+    }
+    .rating-value .gray-text {
+        margin-left: 7px;
+    }
+    .rating-value {
+        display: flex;
+        margin-top: 10px;
+        margin-right: 12px;
+        color: #6074E8;
+    }
+    h3 {
+        margin-bottom: 0;
+        text-align: left;
+    }
+    .profile-rating >>>.el-rate__icon{
+        font-size: 28px;
+    }
+    .profile-rating {
+        height: 30px;
+        display: flex;
+        align-items: center;
+    }
+    .add-pet-text {
+        display: flex;
+        flex-direction: column;
+    }
+    .text {
+
+    }
+    .gray-text {
+        color: #989DAC;
+
+    }
+    >>>.el-card {
+        border-radius: 8px;
+        padding: 0;
+    }
+    
+    >>>.el-card:first-of-type {
+        padding: 10px;
+    }
+
+    >>>.el-card:not(:first-of-type) {
+        margin-top: 32px;
+    }
+    >>>.el-button {
+        width: 48px;
+        height: 48px;
+    }
+    .add-pet {
+        margin-top: 32px;
+        height: 140px;
+        background: #EDEEF0;
+        border: 1px dashed #989DAC;
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    img {
+        width: 100%;
+    }
+    >>>.el-card__body {
+        padding-left: 10px;
+        padding-top: 17px;
+    }
+
+    >>>.pet-profile-header > .el-card__body {
+        display: flex;
+        justify-content: space-between;
+        padding: 0;
+    }
+
+    .pet-photo-wrapper {
+        display: inline-flex;
+        position: relative;
+        width: 85px;
+        height: 85px;
+        border-radius: 50%;
+        box-shadow: 0 0 5px rgba(22, 36, 83, 0.3);
+        margin: auto;
+        margin-left: 5px;
+    }
+    
+    .pet-photo {
+        width: 100%;
+        border-radius: 50%;
+    }
+
+    .pet-profile-header__user {
+        width: 200px;
+    }
+
+    .pet-profile-header__user > *{
+        text-align: left;
+    }
+
+    .pet-profile-header__user-name {
+        margin: 0;
+        text-align: left;
+    }
+
+    .profile-name {
+        margin-bottom: 5px;
+    }
+
+
+    .td--right {
+        width: 210px;
+        height: 44px;
+        text-align: left;
+        line-height: 44px;
+        border-bottom: 1px solid #DFE1E5;
+    }
+
+    .link-btn-wrapper {
+        margin-top: 5px;
+    }
+
+    .link-btn {
+        text-decoration: none;
+        color: #6074E8;
+    }
+
+    .pet-logo-wrapper {
+        width: 48px;
+        height: 48px;
+    }
+
+    .box-card__title {
+        margin: 0;
+        text-align: left;
+    }
+
+    .pet-type {
+        text-align: left;
+        margin-top: 5px;
+    }
+
 
 </style>
